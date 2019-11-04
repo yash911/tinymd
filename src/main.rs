f40c4cbb15fa787f87ac78de9a2d3df31b8331e0
yash911 <ydk61999@gmail.com>
2019-11-04 20:49:09 +0530

    have written 3 functions along with Path variable

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,51 @@
-fn get_title() -> String {
+use std::path::Path;
+use std::fs::File;
 
+fn get_title() -> String {
+    let mut title = String::from(env!("CARGO_PKG_NAME"));
+    title.push_str(" (v");
+    title.push_str(env!("CARGO_PKG_VERSION"));
+    title.push_str("), ");
+    title.push_str(env!("CARGO_PKG_DESCRIPTION"));
+    title
 }
 
-fn parse_markdown_file() {
+fn parse_markdown_file(_filename: &str) {
+    print_short_banner();
+    println!("[ INFO ] Parser firing up!");
+    let input_filename = Path::new(_filename);
+    
 
 }
 
 fn print_short_banner() {
+    println!("{}", get_title());
 
 }
 
 fn print_long_banner() {
+    print_short_banner();
+    let author = String::from(env!("CARGO_PKG_AUTHORS"));
+    let homepage = String::from(env!("CARGO_PKG_HOMEPAGE"));
+    println!("Written by: {}\nHomepage: {}\nUsage: tinymd <somefile>.md",
+            author,
+            homepage
+            );
 
 }
 
 fn usage() {
-
+    print_long_banner();
 }
 
 fn main() {
-    usage();
+    let args: Vec<String> = std::env::args().collect();
 
+    match args.len() {
+        2 => parse_markdown_file(&args[1]),
+        _ => {
+            println!("[ ERROR ] Invalid invocation! ");
+            usage();
+        }
+    }
 }
---
+++ test.md
@@ -0,0 +1,19 @@
+# David Foster Wallace
+
+It’s got something to do with love. 
+With having the discipline to talk 
+out of the part of yourself that 
+can love instead of the part that
+just wants to be loved.
+
+# Ludwig Wittgenstein
+
+The limits of my language means the 
+limit of my world.
+
+# Marcus Aurelius
+
+Very little is needed to make a 
+happy life; it is all within 
+yourself, in your way of thinking.
+
