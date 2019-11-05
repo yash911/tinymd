69a12d249130509c3d714583504b4afe6ef50041
yash911 <ydk61999@gmail.com>
2019-11-06 00:47:57 +0530

    Added release version

---
+++ target/release/deps/tinymd-ed889fd10aa1c49d.d
@@ -0,0 +1,5 @@
+/home/yashdk/Public/rust_projects/tinymd/target/release/deps/tinymd-ed889fd10aa1c49d: src/main.rs
+
+/home/yashdk/Public/rust_projects/tinymd/target/release/deps/tinymd-ed889fd10aa1c49d.d: src/main.rs
+
+src/main.rs:
---
+++ target/release/tinymd.d
@@ -0,0 +1,1 @@
+/home/yashdk/Public/rust_projects/tinymd/target/release/tinymd: /home/yashdk/Public/rust_projects/tinymd/src/main.rs
